493a6a5c02b36f32048eae4b9228e0ecf5e29d90
changeuikim <cekim.dev@gmail.com>
2024-10-04 22:58:29 +0900

    [chore] RootLayout 설정 변경
    - 폰트를 Pretendard로 변경
    - 사이트의 메타데이터를 별도의 파일로 관리
    - html, body, main 태그의 CSS를 변경
    - TailwindCSS 설정 파일에 스타일링을 적용할 컨텐츠 경로를 추가

---
+++ src/data/siteMetadata.js
@@ -0,0 +1,14 @@
+export const siteMetadata = {
+  title: 'Kayce의 기술 블로그',
+  author: '김창의',
+  headerTitle: 'KayceTechBlog',
+  description: '웹 개발을 하면서 알게된 것과 느낀 점을 기록합니다.',
+  language: 'en-us',
+  theme: 'system',
+  siteUrl: 'https://kayce.vercel.app/',
+  siteRepo: 'https://github.com/changeuikim/kayce',
+  email: 'cekim.dev@gmail.com',
+  github: 'https://github.com/changeuikim',
+  linkedin: 'https://www.linkedin.com/in/kim-changeui-b81a9b330/',
+  instagram: 'https://www.instagram.com/changeui_kim/',
+};
